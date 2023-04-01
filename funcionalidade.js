92df038bfece65518fa3f93c068ae316f755a27a
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-03-31 21:14:54 -0300

    Botões e HTML otimizado

--- funcionalidade.js
+++ funcionalidade.js
@@ -4,8 +4,8 @@ function marque1(seletor) {
         //remove a classe selecionado desse botao
         botaomarcado.classList.remove('marcado1');
     }
-    let elemento1 = document.querySelector(seletor);
-	elemento1.classList.toggle("marcado1");
+	seletor.classList.toggle("marcado1");
+    check();
 }
 function marque2(seletor) {
     const botaomarcado = document.querySelector('.marcado2');
@@ -13,8 +13,8 @@ function marque2(seletor) {
         //remove a classe selecionado desse botao
         botaomarcado.classList.remove('marcado2');
     }
-    let elemento1 = document.querySelector(seletor);
-	elemento1.classList.toggle("marcado2");
+	seletor.classList.toggle("marcado2");
+    check();
 }
 function marque3(seletor) {
     const botaomarcado = document.querySelector('.marcado3');
@@ -22,10 +22,19 @@ function marque3(seletor) {
         //remove a classe selecionado desse botao
         botaomarcado.classList.remove('marcado3');
     }
-    let elemento1 = document.querySelector(seletor);
-	elemento1.classList.toggle("marcado3");
+	seletor.classList.toggle("marcado3");
+    check();
 }
 
-const marcador1 = document.querySelector('.marcado1');
-const marcador2 = document.querySelector('.marcado2');
-const marcador3 = document.querySelector('.marcado3');
+
+function check() {
+    const marcador1 = document.querySelector('.marcado1');
+    const marcador2 = document.querySelector('.marcado2');
+    const marcador3 = document.querySelector('.marcado3');
+    if (marcador1 !== null && marcador2 !== null && marcador3 !== null  ) {
+    const finalizador = document.querySelector('.finalizar-pedido');
+    finalizador.classList.add('final-pronto');
+    finalizador.disabled = false;
+    finalizador.innerHTML  = "Fechar Pedido";
+    }
+}
